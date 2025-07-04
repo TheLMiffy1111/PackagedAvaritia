341113697261903f8135096a4385cdc605be8c60
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-07-04 14:34:57 +0800

    Adjust block strength values

--- src/main/java/thelm/packagedavaritia/block/BlockExtremeCrafter.java
+++ src/main/java/thelm/packagedavaritia/block/BlockExtremeCrafter.java
@@ -22,7 +22,7 @@ public class BlockExtremeCrafter extends BlockBase {
 
 	public BlockExtremeCrafter() {
 		super(Material.IRON);
-		setHardness(15F);
+		setHardness(10F);
 		setResistance(25F);
 		setSoundType(SoundType.METAL);
 		setTranslationKey("packagedavaritia.extreme_crafter");
