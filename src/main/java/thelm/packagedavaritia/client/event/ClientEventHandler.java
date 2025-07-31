db6e0557d9a55cfff438ad5f3708f1f141de3cf9
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-08-01 04:26:23 +0800

    Update Forge

--- src/main/java/thelm/packagedavaritia/client/event/ClientEventHandler.java
+++ src/main/java/thelm/packagedavaritia/client/event/ClientEventHandler.java
@@ -21,6 +21,7 @@ public class ClientEventHandler {
 		return INSTANCE;
 	}
 
+	@SuppressWarnings("removal")
 	public void onConstruct() {
 		FMLJavaModLoadingContext.get().getModEventBus().register(this);
 	}
